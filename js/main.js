22ddb44d7b7f9b54b39d8dc18d7b74aa641a4d3b
FairyFgt <57915182+FairyFgt@users.noreply.github.com>
2020-03-04 14:40:56 +0100

    I am soon unbanned on GTA online :)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,91 @@
 var request = new XMLHttpRequest();
-var element = document.querySelector("")
+var mag_types = document.querySelector(".mag_types")
+var mag_stats = document.querySelector(".mag_stats")
 
-element.innerHTML= "RAWR X3"
-
-request.open("GET", "https://pokeapi.co/api/v2/pokemon/4");
+request.open("GET", "https://pokeapi.co/api/v2/pokemon/magikarp");
 
 request.onload = function(){
     var data = JSON.parse(this.response);
-    console.log(data.name);
+
+    var type_iteration = 0
+    while(type_iteration < data.types.length){
+        var p = document.createElement("p")
+        p.innerHTML = data.types[type_iteration].type.name
+        mag_types.appendChild(p)
+        type_iteration++
+    }
+
+    var i = 0
+
+    while(i < data.stats.length) {
+        var p = document.createElement('p')
+        p.innerHTML = data.stats[i].stat.name
+        mag_stats.appendChild(p)
+        
+        var p2 = document.createElement('p')
+        p2.innerHTML = data.stats[i].base_stat
+        mag_stats.appendChild(p2)
+
+        i++
+    }
 }
 
 request.send();
+
+var poke_list = new XMLHttpRequest();
+poke_list.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=99999");
+
+poke_list.onload = function(){
+    var data = JSON.parse(this.response)
+    console.log(data)
+    var i = 0
+    while(i < data.results.length){
+        var option = document.createElement("option")
+        option.innerHTML = data.results[i].name
+        poke_select.appendChild(option)
+
+        i++
+    }
+
+}
+
+poke_list.send()
+
+var comparison_types = document.querySelector(".comparison_types")
+var comparison_stats = document.querySelector(".comparison_stats")
+var comparison_pic = document.querySelector(".comparison_pic")
+
+document.querySelector("#poke_select").addEventListener("input", function(){
+    var pokemon = this.value
+    
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://pokeapi.co/api/v2/pokemon/" + pokemon);
+    request.onload = function(){
+        var data = JSON.parse(this.response);
+        var type_iteration = 0
+        while(type_iteration < data.types.length){
+            var p = document.createElement("p")
+            p.innerHTML = data.types[type_iteration].type.name
+            comparison_types.appendChild(p)
+            type_iteration++
+
+        }
+        comparison_stats.innerHTML = ''
+        comparison_pic.src = data.sprites.front_default
+        var i = 0
+
+        while(i < data.stats.length) {
+            var p = document.createElement('p')
+            p.innerHTML = data.stats[i].stat.name
+            comparison_stats.appendChild(p)
+            
+            var p2 = document.createElement('p')
+            p2.innerHTML = data.stats[i].base_stat
+            comparison_stats.appendChild(p2)
+
+            i++
+        }
+    }
+
+    request.send();
+})
