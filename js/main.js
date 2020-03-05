4a2a0892f282eaafacdff4d5dfa5b9030b8def4e
FairyFgt <57915182+FairyFgt@users.noreply.github.com>
2020-03-05 14:02:31 +0100

    n

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 var request = new XMLHttpRequest();
 var mag_types = document.querySelector(".mag_types")
 var mag_stats = document.querySelector(".mag_stats")
+var points_res = document.querySelector("#pointsResult");
+
+var magikarp_data = {
+    types: [],
+    base_stat: []
+}
+
 
 request.open("GET", "https://pokeapi.co/api/v2/pokemon/magikarp");
 
 request.onload = function(){
     var data = JSON.parse(this.response);
-
     var type_iteration = 0
     while(type_iteration < data.types.length){
         var p = document.createElement("p")
         p.innerHTML = data.types[type_iteration].type.name
+        magikarp_data.types.push(data.types[type_iteration].type.name)
         mag_types.appendChild(p)
         type_iteration++
     }
@@ -24,6 +31,7 @@ request.onload = function(){
         
         var p2 = document.createElement('p')
         p2.innerHTML = data.stats[i].base_stat
+        magikarp_data.base_stat.push(data.stats[i].base_stat)
         mag_stats.appendChild(p2)
 
         i++
@@ -62,17 +70,17 @@ document.querySelector("#poke_select").addEventListener("input", function(){
     request.onload = function(){
         var data = JSON.parse(this.response);
         var type_iteration = 0
+        comparison_types.innerHTML = ''
         while(type_iteration < data.types.length){
             var p = document.createElement("p")
             p.innerHTML = data.types[type_iteration].type.name
             comparison_types.appendChild(p)
-            type_iteration++
 
         }
         comparison_stats.innerHTML = ''
         comparison_pic.src = data.sprites.front_default
         var i = 0
-
+        var points = 0
         while(i < data.stats.length) {
             var p = document.createElement('h4')
             p.innerHTML = data.stats[i].stat.name
@@ -81,14 +89,25 @@ document.querySelector("#poke_select").addEventListener("input", function(){
             var p2 = document.createElement('p')
             p2.innerHTML = data.stats[i].base_stat
             comparison_stats.appendChild(p2)
+            if(data.stats[i].base_stat < magikarp_data.base_stat[i]){
+                points += 1
+            }else if(data.stats[i].base_stat > magikarp_data.base_stat[i]){
+                points -= 1
+            }
 
             i++
         }
+        points_res.innerHTML = points
+    
     }
 
     request.send();
 })
 
+
+
+
+/*
 var comparison_points = new XMLHttpRequest();
 comparison_points.open("GET", "https://pokeapi.co/api/v2/pokemon");
 
@@ -99,3 +118,26 @@ comparison_points.onload = function(){
    while(i < data.)
 
 }
+*/
+
+
+/*
+Straight out of Github
+Function moves the background while scrolling, also scaling it all whilst making it move smoother
+*/
+
+function Scroll_bg() {
+    var window_height = window.innerHeight;
+    var window_width = window.innerWidth;
+    var scroll = (window.pageYOffset) / (document.body.clientHeight - window_height);
+    var push_distance = -scroll * (window_height/5);
+    if (window_width > 590) {
+        document.body.style.background = 'url("img/water.jpg") 0px ' + push_distance + 'px / 100vw 120vh no-repeat fixed';
+    } else {
+        document.body.style.background = 'url("img/water.jpg") 0px ' + push_distance + 'px / auto 120vh no-repeat fixed';
+    }
+}
+
+window.addEventListener('scroll', Scroll_bg);
+window.addEventListener('resize', Scroll_bg);
+Scroll_bg();
